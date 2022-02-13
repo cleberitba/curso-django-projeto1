2318734a2469bbd016258867752615babb86612a
cleber costa <clebim.1@gmail.com>
2022-02-13 15:18:01 -0300

    creation of recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(resquet):
+    return HttpResponse('ooooo')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view),
+
 ]
